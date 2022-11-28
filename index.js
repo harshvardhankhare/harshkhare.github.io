74b2455c30ea8d5520453e676c642d03a5b3e12c
harshvardhankhare <119111450+harshvardhankhare@users.noreply.github.com>
2022-11-28 23:42:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("script rummong")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener('click',()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+})
---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelector('.ham').style.display='none';
